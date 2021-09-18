eadab1b399c7ff420dcf872f862e5d8073bf2563
Mayank242002 <msngi24july@gmail.com>
2021-09-18 17:30:25 +0530

    Applied some styles and make features like minimum and maximum temperature

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Weather from './components/weather';
 function App() {
   return (
     <div>
+      <h1 className="heading">Weather App </h1>
+      <p className="para"> A weather app to find the weather of any City of the world.</p>
   <Weather/>
     </div>
   );
--- src/components/weather.js
+++ src/components/weather.js
@@ -6,17 +6,17 @@ const handlechange =()  =>{
 }
 const Weather = () =>{
       const [city,setCity]=useState(null);
-      const [search,setSearch]=useState("Mumbai")
+      const [search,setSearch]=useState("Dehradun")
 
       useEffect (()=>{
 
-        const url=`http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=c330fc44a97066547ea044f31c873fde`;
+        const url=`http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=c330fc44a97066547ea044f31c873fde`;
 
         const fetchData=async()=>{
           try{
             const response=await fetch(url);
             const jsonData=await response.json();
-            setCity(jsonData.name);
+            setCity(jsonData.main);
           }catch(error){
             console.log(error);
           }
@@ -24,25 +24,35 @@ const Weather = () =>{
 
         fetchData();
         
-      },[]);
+      },[search]);
+
+      const handlechange=(event)=>{
+        setSearch(event.target.value);
+      }
 
     return (
      <div className="box">
         <div className="inputData"> 
-        <input type="search" className="inputField" onChange={handlechange}/>   
-        
+        <input type="search" value={search} className="inputField" onChange={handlechange}/>     
      </div>
-     <div className="info"> 
+     {!city?(
+       <p className="errorMsg">NO Data Found</p>  
+     ):(<div> 
+    <div className="info"> 
        <h2 className="location">
-     <i className="fas fa-street-view">{city}</i> 
+        <i className="fas fa-street-view"></i> {search}
        </h2>
        <h1 className="temp">
+         {city.temp}°C
        </h1>
-       <h3 className="tempmin_max"> </h3>
+       <h3 className="tempmin_max"> Min : ${city.temp_min}°C || Max : ${city.temp_max}°C </h3>
      </div>
+
      <div className="wave -one">  </div>
      <div className="wave -one">  </div>
      <div className="wave -one">  </div>
+     </div>)}
+     
      </div>
     
     );
